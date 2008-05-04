21532d02e52d98dd33c1a5a074933e30a5b50b35
Albert Astals Cid <aacid@kde.org>
2008-05-04 15:26:26 +0200

    Fix leaks on error conditions

--- poppler/Annot.cc
+++ poppler/Annot.cc
@@ -2095,6 +2095,9 @@ void AnnotWidget::drawText(GooString *text, GooString *da, GfxFontDict *fontDict
     error(-1, "Missing 'Tf' operator in field's DA string");
   }
   if (!font) {
+    if (daToks) {
+      deleteGooList(daToks, GooString);
+    }
     return;
   }
 
@@ -2448,6 +2451,9 @@ void AnnotWidget::drawListBox(GooString **text, GBool *selection,
     error(-1, "Missing 'Tf' operator in field's DA string");
   }
   if (!font) {
+    if (daToks) {
+      deleteGooList(daToks, GooString);
+    }
     return;
   }
 
