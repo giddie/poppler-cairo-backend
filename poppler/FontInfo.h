eacb7940b53ec3c026afa57103221681ca3fde36
Albert Astals Cid <aacid@kde.org>
2008-07-22 21:05:03 +0200

    Some documents have loops in XObject dictionaries, make sure we don't get in an infinite loop while traversing them
    Fixes infinite loop on http://bugs.kde.org/show_bug.cgi?id=166145

--- poppler/FontInfo.h
+++ poppler/FontInfo.h
@@ -63,6 +63,10 @@ private:
   int fontsLen;
   int fontsSize;
 
+  Ref *visitedXObjects;
+  int visitedXObjectsLen;
+  int visitedXObjectsSize;
+
   void scanFonts(Dict *resDict, GooList *fontsList);
 };
 
