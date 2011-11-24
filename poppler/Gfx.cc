tsdgeos/poppler_mirror

commit f6d026bfa18624ccd321e102bb39ba744998de1e

Do not crash if failing to parse the colorspace
Fixes bug 42793

poppler/Gfx.cc
@@ -1319,7 +1319,7 @@ void Gfx::opSetRenderingIntent(Object args[], int numArgs) {
 
 void Gfx::opSetFillGray(Object args[], int numArgs) {
   GfxColor color;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   Object obj;
 
   if (textHaveCSPattern && drawText) {
@@ -1332,10 +1332,11 @@ void Gfx::opSetFillGray(Object args[], int numArgs) {
   }
   state->setFillPattern(NULL);
   res->lookupColorSpace("DefaultGray", &obj);
-  if (obj.isNull()) {
-    colorSpace = new GfxDeviceGrayColorSpace();
-  } else {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceGrayColorSpace();
   }
   obj.free();
   state->setFillColorSpace(colorSpace);
@@ -1354,15 +1355,16 @@ void Gfx::opSetFillGray(Object args[], int numArgs) {
 
 void Gfx::opSetStrokeGray(Object args[], int numArgs) {
   GfxColor color;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   Object obj;
 
   state->setStrokePattern(NULL);
   res->lookupColorSpace("DefaultGray", &obj);
-  if (obj.isNull()) {
-    colorSpace = new GfxDeviceGrayColorSpace();
-  } else {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceGrayColorSpace();
   }
   obj.free();
   state->setStrokeColorSpace(colorSpace);
@@ -1374,7 +1376,7 @@ void Gfx::opSetStrokeGray(Object args[], int numArgs) {
 
 void Gfx::opSetFillCMYKColor(Object args[], int numArgs) {
   GfxColor color;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   Object obj;
   int i;
 
@@ -1387,10 +1389,11 @@ void Gfx::opSetFillCMYKColor(Object args[], int numArgs) {
     out->restoreState(state);
   }
   res->lookupColorSpace("DefaultCMYK", &obj);
-  if (obj.isNull()) {
-    colorSpace = new GfxDeviceCMYKColorSpace();
-  } else {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceCMYKColorSpace();
   }
   obj.free();
   state->setFillPattern(NULL);
@@ -1412,16 +1415,17 @@ void Gfx::opSetFillCMYKColor(Object args[], int numArgs) {
 
 void Gfx::opSetStrokeCMYKColor(Object args[], int numArgs) {
   GfxColor color;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   Object obj;
   int i;
 
   state->setStrokePattern(NULL);
   res->lookupColorSpace("DefaultCMYK", &obj);
-  if (obj.isNull()) {
-    colorSpace = new GfxDeviceCMYKColorSpace();
-  } else {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceCMYKColorSpace();
   }
   obj.free();
   state->setStrokeColorSpace(colorSpace);
@@ -1435,7 +1439,7 @@ void Gfx::opSetStrokeCMYKColor(Object args[], int numArgs) {
 
 void Gfx::opSetFillRGBColor(Object args[], int numArgs) {
   Object obj;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   GfxColor color;
   int i;
 
@@ -1450,9 +1454,10 @@ void Gfx::opSetFillRGBColor(Object args[], int numArgs) {
   state->setFillPattern(NULL);
   res->lookupColorSpace("DefaultRGB", &obj);
   if (obj.isNull()) {
-    colorSpace = new GfxDeviceRGBColorSpace();
-  } else {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceRGBColorSpace();
   }
   obj.free();
   state->setFillColorSpace(colorSpace);
@@ -1473,16 +1478,17 @@ void Gfx::opSetFillRGBColor(Object args[], int numArgs) {
 
 void Gfx::opSetStrokeRGBColor(Object args[], int numArgs) {
   Object obj;
-  GfxColorSpace *colorSpace;
+  GfxColorSpace *colorSpace = NULL;
   GfxColor color;
   int i;
 
   state->setStrokePattern(NULL);
   res->lookupColorSpace("DefaultRGB", &obj);
-  if (obj.isNull()) {
-    colorSpace = new GfxDeviceRGBColorSpace();
-  } else {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
+  }
+  if (colorSpace == NULL) {
+    colorSpace = new GfxDeviceRGBColorSpace();
   }
   obj.free();
   state->setStrokeColorSpace(colorSpace);

giddie/poppler-cairo-backend

commit 51e5a67c913c0d6c83f5f6b358b7fca7bb5584e1

Fix typo/regression introduced in f6d026bfa18624ccd321e102bb39ba744998de1e (cherry picked from commit bdb439ac5a751d6146204ae0d61fdbf7828c89c3)

poppler/Gfx.cc
@@ -33,6 +33,7 @@
 // Copyright (C) 2009, 2010 David Benjamin <davidben@mit.edu>
 // Copyright (C) 2010 Nils Höglund <nils.hoglund@gmail.com>
 // Copyright (C) 2010 Christian Feuersänger <cfeuersaenger@googlemail.com>
+// Copyright (C) 2011 Axel Strübing <axel.struebing@freenet.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -1453,7 +1454,7 @@ void Gfx::opSetFillRGBColor(Object args[], int numArgs) {
   }
   state->setFillPattern(NULL);
   res->lookupColorSpace("DefaultRGB", &obj);
-  if (obj.isNull()) {
+  if (!obj.isNull()) {
     colorSpace = GfxColorSpace::parse(&obj, this);
   }
   if (colorSpace == NULL) {