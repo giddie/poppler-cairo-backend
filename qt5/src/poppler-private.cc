cbb4723eecd9b52c6cf4ae2260407c5ca739c08b
Paul Gideon Dann <pdgiddie@gmail.com>
2018-04-04 16:37:38 +0100

    Setting default Qt5 backend to Cairo

--- qt5/src/poppler-private.cc
+++ qt5/src/poppler-private.cc
@@ -268,7 +268,7 @@ DocumentData::~DocumentData()
 
 void DocumentData::init()
 {
-    m_backend = Document::SplashBackend;
+    m_backend = Document::CairoBackend;
     paperColor = Qt::white;
     m_hints = 0;
     m_optContentModel = nullptr;
