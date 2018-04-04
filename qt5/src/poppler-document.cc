d20a84c400e44b408c561dbdac5c2a9c219b59ad
Paul Gideon Dann <pdgiddie@gmail.com>
2018-04-04 16:36:56 +0100

    Cairo backend added to Qt5 wrapper

--- qt5/src/poppler-document.cc
+++ qt5/src/poppler-document.cc
@@ -712,6 +712,9 @@ QSet<Document::RenderBackend> Document::availableRenderBackends()
     ret << Document::SplashBackend;
     ret << Document::QPainterBackend;
     ret << Document::ArthurBackend; // For backward compatibility
+#if defined(HAVE_CAIRO)
+    ret << Document::CairoBackend;
+#endif
     return ret;
 }
 
